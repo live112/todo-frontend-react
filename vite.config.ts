0b87cbe6363a1ebc7508ef88df6271bbbfea24fd
live112 <starlaps03@outlook.com>
2025-11-22 21:45:42 -0600

    .

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), tailwindcss()],
-  base: "/podcast-frontend-react/",
+  base: "/todo-frontend-react/",
   resolve: {
     alias: {
       "@api": fileURLToPath(new URL("./src/api", import.meta.url)),
